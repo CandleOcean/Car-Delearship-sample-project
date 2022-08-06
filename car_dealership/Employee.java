d1cc565691748d1adf2f1ea9e1c3b2baee79b039
Candle Ocean <oceancandle@gmail.com>
2022-08-06 22:53:33 +0300

    Fixed bugs in my project

--- car_dealership/Employee.java
+++ car_dealership/Employee.java
@@ -1,13 +1,12 @@
 package car_dealership;
 
-public class Employee extends Customer{
-	String name;
+public class Employee extends Person{
 	
-	public void handleCustomer(Customer cust, boolean finance, Vehicle vehicle) {
-		if(finance == true) {
-			runCreditHistory(cust1.getName(), cust1.getCashOnHand());
-		}else if(vehicle.getPrice() <= cust1.getCashOnHand()) {
-			processTransaction(cust1.getName(), vehicle.getName());
+	public void handleCustomer(Customer cust, boolean isFinancedPayment, Vehicle vehicle) {
+		if(isFinancedPayment) {
+			runCreditHistory(cust, cust.getCashOnHand());
+		} else if(vehicle.getPrice() <= cust.getCashOnHand()) {
+			processTransaction(cust, vehicle);
 			
 		}else {
 			System.out.println("Add on your money to get yourself a car of your choice");
@@ -15,11 +14,12 @@ public class Employee extends Customer{
 	}
 	
 	public void runCreditHistory(Customer cust, double loanAmount) {
-		System.out.println(cust1.getName()+" just bought himself a " + vehicle1.getName);
+		System.out.println("Running the customers credit history");
 	}
 	
 	public void processTransaction(Customer cust1, Vehicle vehicle) {
-		
+		System.out.println(cust1.getName()+" just bought himself a " + vehicle.getName());
+
 	}
 
 }
---
+++ car_dealership/Person.java
@@ -0,0 +1,32 @@
+package car_dealership;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person() {
+    }
+
+    //POJO  - Plain Java Object
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
